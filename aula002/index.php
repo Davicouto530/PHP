8faaed04d4c282494c05a691d0658e64c421beae
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-08-24 15:20:12 -0300

    Saída de dados com echo() e print().

---
+++ aula002/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saída de dados</title>
+</head>
+<body>
+    <?php 
+
+        // Comando "echo" para impressão de dados no html, serve só para a saída de dados
+        echo 'Comando echo <br/>';
+
+        // Comando "print" para impressão de dados no html. O comando "print" é uma função em php, que gera um retorno e exibindo esse retorno na página html com o "echo".
+        // Quando a função é retornada corretamente, ela retorna "1", quando existe algum erro na impressão, retorna "0".
+        echo print ('Comando print <hr/>')
+    ?>
+</body>
+</html>
