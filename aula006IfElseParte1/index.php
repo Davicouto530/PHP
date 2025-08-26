dbf2fd617a31043731c1320e210991f0d7a2429e
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-08-26 19:39:48 -0300

    Aula sobre if/else com operadores condicionais.

---
+++ aula006IfElseParte1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If/Else operadores de condicionais</title>
+</head>
+<body>
+    <h1>If/Else operadores de condicionais</h1>
+
+    <?php 
+
+        // == -> Verifica se os valores são iguais
+        // === -> Verifica se os valores e o tipo são iguais
+        // != ou <> -> Verifica se os valores são diferentes
+        // !== -> Verifica se os valores são diferentes e de tipos diferentes
+        // < -> Verifica se o valor da esquerda é menor que o valor da direta
+        // > -> Verifica se o valor da esquerda é maior que o valor da direta
+        // <= -> Verifica se o valor da esquerda é menor ou igual que o valor da direta
+        // >= -> Verifica se o valor da esquerda é maior ou igual que o valor da direta
+
+        if(2 !== 2) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+
+    ?>
+</body>
+</html>
